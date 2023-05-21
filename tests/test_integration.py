a78e1d3a6a4820db5fe31e626a6b2d8532ee6362
Cristian Garcia <cgarcia.e88@gmail.com>
2023-05-21 18:43:35 +0000

    fix integration test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -8,35 +8,38 @@ import nnx
 class TestIntegration:
     def test_shared_modules(self):
         class Block(nnx.Module):
-            def __init__(self, linear: nnx.Linear):
+            def __init__(self, linear: nnx.Linear, *, rngs: nnx.Rngs):
                 self.linear = linear
-                self.bn = nnx.BatchNorm(2)
+                self.bn = nnx.BatchNorm(2, rngs=rngs)
 
-            def __call__(self, x):
-                return nnx.relu(self.bn(self.linear(x)))
+            def __call__(self, x, *, train: bool):
+                x = self.linear(x)
+                x = self.bn(x, use_running_average=not train)
+                return nnx.relu(x)
 
         class Model(nnx.Module):
-            def __init__(self):
-                shared = nnx.Linear(2, 2)
-                self.block1 = Block(shared)
-                self.block2 = Block(shared)
+            def __init__(self, *, rngs: nnx.Rngs):
+                shared = nnx.Linear(2, 2, rngs=rngs)
+                self.block1 = Block(shared, rngs=rngs)
+                self.block2 = Block(shared, rngs=rngs)
 
-            def __call__(self, x):
-                x = self.block1(x)
-                x = self.block2(x)
+            def __call__(self, x, *, train: bool):
+                x = self.block1(x, train=train)
+                x = self.block2(x, train=train)
                 return x
 
         @nnx.jit
         def train_step(model: Model, x, y):
             @nnx.grad
             def loss_fn(model: Model):
-                y_pred = model.apply(use_running_average=False)(x)
+                y_pred = model(x, train=True)
                 return jnp.mean((y - y_pred) ** 2)
 
             grad = loss_fn(model)
             model[...] = jax.tree_map(lambda w, g: w - 0.1 * g, model["params"], grad)
 
-        model = Model.init(jax.random.PRNGKey(0))()
+        rngs = nnx.Rngs(jax.random.PRNGKey(0))
+        model = Model(rngs=rngs)
 
         x = np.random.uniform(size=(4, 2))
         y = np.random.uniform(size=(4, 2))
@@ -52,29 +55,31 @@ class TestIntegration:
 
     def test_shared_modules_jit_filter(self):
         class Block(nnx.Module):
-            def __init__(self, linear: nnx.Linear):
+            def __init__(self, linear: nnx.Linear, *, rngs: nnx.Rngs):
                 self.linear = linear
-                self.bn = nnx.BatchNorm(2)
+                self.bn = nnx.BatchNorm(2, rngs=rngs)
 
-            def __call__(self, x):
-                return nnx.relu(self.bn(self.linear(x)))
+            def __call__(self, x, *, train: bool):
+                x = self.linear(x)
+                x = self.bn(x, use_running_average=not train)
+                return nnx.relu(x)
 
         class Model(nnx.Module):
-            def __init__(self):
-                shared = nnx.Linear(2, 2)
-                self.block1 = Block(shared)
-                self.block2 = Block(shared)
+            def __init__(self, *, rngs: nnx.Rngs):
+                shared = nnx.Linear(2, 2, rngs=rngs)
+                self.block1 = Block(shared, rngs=rngs)
+                self.block2 = Block(shared, rngs=rngs)
 
-            def __call__(self, x):
-                x = self.block1(x)
-                x = self.block2(x)
+            def __call__(self, x, *, train: bool):
+                x = self.block1(x, train=train)
+                x = self.block2(x, train=train)
                 return x
 
         @nnx.jit_filter
         def train_step(model: Model, x, y):
             @nnx.grad
             def loss_fn(model: Model):
-                y_pred = model.apply(use_running_average=False)(x)
+                y_pred = model(x, train=True)
                 return jnp.mean((y - y_pred) ** 2)
 
             grad = loss_fn(model)
@@ -82,7 +87,8 @@ class TestIntegration:
 
             return model
 
-        model = Model.init(jax.random.PRNGKey(0))()
+        rngs = nnx.Rngs(jax.random.PRNGKey(0))
+        model = Model(rngs=rngs)
 
         x = np.random.uniform(size=(4, 2))
         y = np.random.uniform(size=(4, 2))
