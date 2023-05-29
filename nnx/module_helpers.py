85f26f5c4abdcb04e203a2e280efba36975befad
Cristian Garcia <cgarcia.e88@gmail.com>
2023-05-29 18:47:53 +0000

    remove use of setattr and getattr

--- nnx/module_helpers.py
+++ nnx/module_helpers.py
@@ -6,31 +6,31 @@ A = tp.TypeVar("A")
 
 class Map(Module, tp.Mapping[str, A]):
     def __init__(self, *args, **kwargs):
-        vars(self).update(dict(*args, **kwargs))
+        vars(self).update(*args, **kwargs)
 
-    def __getitem__(self, key):
-        return getattr(self, key)
+    def __getitem__(self, key) -> A:
+        return vars(self)[key]
 
-    def __iter__(self):
+    def __iter__(self) -> tp.Iterator[str]:
         return iter(vars(self))
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(vars(self))
 
 
 class Seq(Module, tp.Sequence[A]):
     def __init__(self, iterable: tp.Iterable[A]):
         for i, value in enumerate(iterable):
-            setattr(self, str(i), value)
+            vars(self)[str(i)] = value
 
-    def __getitem__(self, key: int):
+    def __getitem__(self, key: int) -> A:
         if key >= len(self):
             raise IndexError(f"Index {key} out of range for {self}")
-        return getattr(self, str(key))
+        return vars(self)[str(key)]
 
     def __iter__(self) -> tp.Iterator[A]:
         for i in range(len(self)):
-            yield getattr(self, str(i))
+            yield vars(self)[str(i)]
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(vars(self))
