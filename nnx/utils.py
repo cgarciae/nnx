2f495017cd4e2cc1b8e2c8d093be9f8ae4911085
Cristian Garcia <cgarcia.e88@gmail.com>
2023-05-29 22:19:10 +0000

    clean utils

--- nnx/utils.py
+++ nnx/utils.py
@@ -1,15 +1,6 @@
-import contextlib
 import typing as tp
-import inspect
 
 A = tp.TypeVar("A")
-
-
-@contextlib.contextmanager
-def contexts(*contexts: tp.ContextManager[tp.Any]) -> tp.Iterator[tp.Tuple[tp.Any]]:
-    """Context manager that enters multiple contexts."""
-    with contextlib.ExitStack() as stack:
-        yield tuple(stack.enter_context(context) for context in contexts)
 
 
 def first_from(*args: tp.Optional[A]) -> A:
@@ -18,11 +9,3 @@ def first_from(*args: tp.Optional[A]) -> A:
         if arg is not None:
             return arg
     raise ValueError("No non-None arguments found.")
-
-
-def has_kwarg(fn: tp.Callable[..., tp.Any], kwarg: str) -> bool:
-    """Return True if the function has the given keyword argument."""
-    parameters = inspect.signature(fn).parameters
-    return (
-        kwarg in parameters and parameters[kwarg].kind == inspect.Parameter.KEYWORD_ONLY
-    )
