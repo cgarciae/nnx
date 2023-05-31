2ac5dbc72fdb9d542c23ba61a8ed4684631883d2
Cristian Garcia <cgarcia.e88@gmail.com>
2023-05-31 02:24:07 +0000

    demo

---
+++ examples/00_demo.py
@@ -0,0 +1,26 @@
+# 1. Shared leafs vs shared Modules?
+# 2. Explicit vs sugar syntax?
+
+import jax
+import jax.numpy as jnp
+import nnx
+
+
+class Linear(nnx.Module):
+    def __init__(self, din: int, dout: int, *, ctx: nnx.Context):
+        self.w = nnx.param(jax.random.uniform(ctx.make_rng("params"), (din, dout)))
+        self.b = nnx.param(jnp.zeros((dout,)))
+        self.list = nnx.Seq([nnx.Conv(10, 20, [3, 3], ctx=ctx), 2, 3])
+
+    def __call__(self, x):
+        self.interm = nnx.var("interm", 100)
+        return x @ self.w + self.b
+
+
+ctx = nnx.Context(jax.random.PRNGKey(0))
+linear = Linear(2, 2, ctx=ctx)
+
+
+state, moddef = statedef = linear.deref()
+
+print(state)
