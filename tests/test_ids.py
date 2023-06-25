51c00f8e2ee8853aa66b7052817049936d741864
Cristian Garcia <cgarcia.e88@gmail.com>
2023-06-25 01:06:47 +0000

    add tests for UUID

---
+++ tests/test_ids.py
@@ -0,0 +1,16 @@
+import copy
+
+from nnx import ids
+
+
+class TestIds:
+    def test_hashable(self):
+        id1 = ids.uuid()
+        id2 = ids.uuid()
+        assert id1 == id1
+        assert id1 != id2
+        assert hash(id1) != hash(id2)
+        id1c = copy.copy(id1)
+        id1dc = copy.deepcopy(id1)
+        assert hash(id1) != hash(id1c)
+        assert hash(id1) != hash(id1dc)
